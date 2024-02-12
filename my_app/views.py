d7dc069e491b6ab87a91488611afdc08b2cb6805
Sergey Sologub <sergey.sologub@rtlabs.ru>
2024-02-12 13:47:19 +0300

    feat: If this is applied, the commit will result in the addition of a new functions cbv model "Post"

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 from my_app.models import Post
@@ -7,6 +7,11 @@ from my_app.models import Post
 
 class PostView(ListView):
     model = Post
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_publish=True)
+        return queryset
 
 
 class PostCreateView(CreateView):
@@ -18,11 +23,20 @@ class PostCreateView(CreateView):
 class PostDetailView(DetailView):
     model = Post
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset=queryset)
+        self.object.views += 1
+        self.object.save()
+        return self.object
+
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = ['title', 'content', 'preview']
-    success_url = reverse_lazy('my_app:list')
+    # success_url = reverse_lazy('my_app:list')
+
+    def get_success_url(self):
+        return reverse('my_app:list', kwargs={'pk': self.object.pk})
 
 
 class PostDeleteView(DeleteView):
